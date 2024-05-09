8366cba73dbaf4b4991c69e19e05983fc80cbe26
wojtekw5 <seta.w07.1999@gmail.com>
2024-05-09 15:08:52 +0200

    first pre form

--- project_ww/app.py
+++ project_ww/app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Zapisz plik")
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
 def home():
-    return "Hello, World!"
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('File uploaded successfully', 'success')
+        return redirect(url_for('home'))
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
