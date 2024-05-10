8deecc488b2012af789ccdabbb8f529d5ff87bd9
wojtekw5 <seta.w07.1999@gmail.com>
2024-05-10 11:59:49 +0200

    login and register template second improved

--- project_ww/app.py
+++ project_ww/app.py
@@ -7,6 +7,9 @@ from flask_migrate import Migrate
 import os
 import uuid
 
+from wtforms.fields.simple import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
@@ -23,6 +26,11 @@ class File(db.Model):
 class UploadFileForm(FlaskForm):
     file = FileField("File")
     submit = SubmitField("Upload File!")
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj się')
 
 @app.route('/', methods=["GET", "POST"])
 @app.route('/home', methods=["GET", "POST"])
@@ -58,6 +66,34 @@ def home():
     # Retrieve all files from the database
     files = File.query.all()
     return render_template('index.html', form=form, files=files)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        # Here you should add code to verify the user credentials
+        # For example, query the user from the database
+        # user = User.query.filter_by(email=email).first()
+        # if user and check_password_hash(user.password, password):
+        #     flash('Logged in successfully.', 'success')
+        #     return redirect(url_for('home'))
+        # else:
+        #     flash('Invalid email or password.', 'danger')
+
+        flash('Login attempt with email: {}'.format(email), 'info')
+        return redirect(url_for('home'))
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Process registration details here
+        flash('Registration successful!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html')
 
 @app.route('/download/<file_id>')
 def download_file(file_id):
